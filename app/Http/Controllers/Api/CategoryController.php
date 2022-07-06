8d13b308c5f29aeb314f539d2b566345c345f27b
Islam KAbbary <islamkabbary@gmail.com>
2022-07-06 10:35:16 +0200

    category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -25,75 +25,75 @@ class CategoryController extends Controller
         }
     }
 
-    // public function show($id)
-    // {
-    //     try {
-    //         $data = Category::findOrFail($id);
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => new CategoryResource($data)], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function show($id)
+    {
+        try {
+            $data = Category::findOrFail($id);
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => new CategoryResource($data)], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 
-    // public function allCompaniesinStreet($id)
-    // {
-    //     try {
-    //         $catCompanies = CompanyHasCategory::where('category_id', $id)->get();
-    //         $companies = [];
-    //         foreach ($catCompanies as $company) {
-    //             $companies[] = Company::findOrFail($company->company_id);
-    //         }
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $companies], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function allCompaniesinStreet($id)
+    {
+        try {
+            $catCompanies = CompanyHasCategory::where('category_id', $id)->get();
+            $companies = [];
+            foreach ($catCompanies as $company) {
+                $companies[] = Company::findOrFail($company->company_id);
+            }
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $companies], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 
-    // public function allCategoriesInCompanies($id)
-    // {
-    //     try {
-    //         $catCompanies = CompanyHasCategory::where('company_id', $id)->get();
-    //         $data = [];
-    //         foreach ($catCompanies as $category) {
-    //             $data[] = Category::findOrFail($category->category_id);
-    //         }
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function allCategoriesInCompanies($id)
+    {
+        try {
+            $catCompanies = CompanyHasCategory::where('company_id', $id)->get();
+            $data = [];
+            foreach ($catCompanies as $category) {
+                $data[] = Category::findOrFail($category->category_id);
+            }
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 
-    // public function allBrandInCategories($id)
-    // {
-    //     try {
-    //         $brandCompanies = BrandHasCategory::where('category_id', $id)->get();
-    //         $data = [];
-    //         foreach ($brandCompanies as $category) {
-    //             $data[] = Brand::findOrFail($category->brand_id);
-    //         }
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function allBrandInCategories($id)
+    {
+        try {
+            $brandCompanies = BrandHasCategory::where('category_id', $id)->get();
+            $data = [];
+            foreach ($brandCompanies as $category) {
+                $data[] = Brand::findOrFail($category->brand_id);
+            }
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 
-    // public function allProductsInBrand($id)
-    // {
-    //     try {
-    //         $data = Product::where('brand_id', $id)->get();
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function allProductsInBrand($id)
+    {
+        try {
+            $data = Product::where('brand_id', $id)->get();
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => $data], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 
-    // public function productDetails($id)
-    // {
-    //     try {
-    //         $data = Product::find($id);
-    //         return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => new ProductResource($data)], Response::HTTP_OK);
-    //     } catch (\Throwable $th) {
-    //         return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
-    //     }
-    // }
+    public function productDetails($id)
+    {
+        try {
+            $data = Product::find($id);
+            return response()->json(['status' => 1, 'code' => 200, 'message' => trans(''), 'data' => new ProductResource($data)], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 0, 'data' => $th->getMessage()], 404);
+        }
+    }
 }
